6fb3296854251df8d22c31a08296ba8d9a143da0
Adiguna <adigunarizqi@gmail.com>
2023-08-10 09:30:44 +0700

    setup config dan model product

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
+// const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+// let corsOptions = {
+//   origin: "http://localhost:8080",
+// };
+
+// enable cors
+// app.use(cors(corsOptions));
 
 //request bertipe json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then((result) => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Cannot Connect to Database", err);
+    process.exit();
+  });
 
 //access endpoint '/'
 //res kirim balik response kepada klien
